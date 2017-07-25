9e3195ab3ba88929759c2566845dd6297aa405a2
jaillantpi <jaillantpi@eisti.eu>
2017-07-25 08:37:53 +0200

    Add files via upload

---
+++ src/affichage/JSlidePanel.java
@@ -0,0 +1,100 @@
+package affichage;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.border.BevelBorder;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class JSlidePanel implements ActionListener {
+
+	private JFrame frame;
+	private JTextArea textField;
+	public static float sliderValue = 0;
+	private JTextPane textPane;
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * Create the application.
+	 */
+	public JSlidePanel() {
+		initialize();
+		
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 306, 76);
+		
+		frame.getContentPane().setLayout(null);
+		
+		
+		JSlider slider = new JSlider();
+		slider.setValue(100);
+		slider.setMaximum(300);
+		slider.setSnapToTicks(true);
+		slider.setToolTipText("\r\n");
+		slider.setBounds(10, 6, 200, 27);
+		frame.getContentPane().add(slider);
+		slider.setMinorTickSpacing(10);
+		slider.setMajorTickSpacing(10);
+		textField = new JTextArea();
+		textField.setFont(new Font("Monospaced", Font.PLAIN, 13));
+		textField.setToolTipText("\r\n\r\n\r\n");
+		textField.setBounds(220, 6, 48, 20);
+		String texte = textField.getText();
+		
+		
+		slider.addChangeListener(new ChangeListener(){
+
+			public void stateChanged(ChangeEvent event){
+				 sliderValue = slider.getValue();
+				 textField.setText(""+sliderValue+"");
+			//	System.out.println("La valeur du Jslide "+sliderValue);
+				
+				Affichage.sliderValue =sliderValue;
+				
+			}
+		});   
+		
+		
+		
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textPane = new JTextPane();
+		textPane.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textPane.setEnabled(false);
+		textPane.setEditable(false);
+		textPane.setText("%");
+		textPane.setBounds(264, 6, 41, 20);
+		frame.getContentPane().add(textPane);
+		
+		frame.setVisible(true);
+	}
+
+	public static float getSliderValue() {
+		return sliderValue;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
